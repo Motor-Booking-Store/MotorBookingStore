3cfea8bfc26fce70082fd36cfef29643879887a4
n p <testuserlmaolol@gmail.com>
2026-03-12 19:39:35 +0900

    remove web.xml and switch to annotation

--- src/java/controller/AdminRentalController.java
+++ src/java/controller/AdminRentalController.java
@@ -9,6 +9,7 @@ import dao.RentalDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,6 +20,7 @@ import model.Rental;
  *
  * @author testu
  */
+@WebServlet("/AdminRentalController")
 public class AdminRentalController extends HttpServlet {
    
     /** 
--- src/java/controller/ApproveRentalController.java
+++ src/java/controller/ApproveRentalController.java
@@ -9,6 +9,7 @@ import dao.RentalDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author testu
  */
+@WebServlet("/ApproveRentalController")
 public class ApproveRentalController extends HttpServlet {
    
     /** 
